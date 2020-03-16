8c898e7bb300264f9c4f2cb6354f9ea7f105856c
Fahmi <pammimajid@gmail.com>
2020-03-16 20:47:03 +0800

    okk

---
+++ tugas_15_js.js
@@ -0,0 +1,6 @@
+var AB = 8
+var BC = 6
+console.log("nilai AB : "+(AB))
+console.log("nilai BC : "+(BC))
+
+console.log("Panjang sisi AC pada segitiga siku siku tersebut adalah : "+Math.sqrt((Math.pow(AB,2)+Math.pow(BC,2)))) 
